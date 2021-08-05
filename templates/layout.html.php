d2acef8d2b323f1e18fd1c8cfe976ac775b7e261
malvr00 <77275513+malvr00@users.noreply.github.com>
2021-08-05 20:48:19 +0900

    layout url change

--- templates/layout.html.php
+++ templates/layout.html.php
@@ -2,17 +2,18 @@
 <html>
   <head>
     <meta charset="utf-8">
-    <link rel="stylesheet" href="../css/main.css">
+    <link rel="stylesheet" href="../css/styles.css">
     <title><?=$title?></title>
   </head>
   <body>
     <header class="main-header">
       <div class="login-bar">
-        <a href="../controllers/signUp.php">Sign Up</a>
+        <a href="../controllers/userInOut.php?signtype=1">Sign Up</a>
         <a href="#">Log In</a>
       </div>
-      <h1><a href="index.php">Auction</a></h1>
+      <h1><a href="../php/index.php">Auction</a></h1>
     </header>
+    
     <nav class="status-bar">
       <ul>
         <li><a href="../controllers/writing.php">User bulletin board </a></li>
@@ -21,9 +22,11 @@
         <li><a href="#">Service center</a></li>
       </ul>
     </nav>
+    
     <main class="main-screen">
       <?=$outString?>
     </main>
+    
     <footer class="footer">
       <h4> 2021. 08 &copy yong il<h4>
     </footer>

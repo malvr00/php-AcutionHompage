d098e0a54c94523b612d5d05301f18103cc79ede
malvr00 <77275513+malvr00@users.noreply.github.com>
2021-08-05 20:47:36 +0900

    DB Query class

---
+++ includes/Userfunction.php
@@ -0,0 +1,23 @@
+<?php
+ class Userfunction{
+  private $pdo;
+  public function __construct($pdo){
+    $this->pdo = $pdo;
+  }
+  public function __destruct(){}
+
+// userTable 정보 가저오는 Qurey
+  public function seachUserId($sql){
+    $stmt = $this->pdo->prepare($sql);
+    $stmt->execute();
+    return $stmt->fetchAll();
+  }
+  
+// Insert 함수
+  public function insertData($sql, $param){
+    $stmt = $this->pdo->prepare($sql);
+    $stmt->execute($param);
+    return $stmt;
+  }
+ }
+ 

9120c3e6b9ca1a98b787093c3dc5ac947ad1e6a3
malvr00 <77275513+malvr00@users.noreply.github.com>
2021-08-20 21:04:30 +0900

    layout html

--- templates/layout.html.php
+++ templates/layout.html.php
@@ -19,7 +19,7 @@
         <li><a href="../controllers/writing.php<?=$user?>">User bulletin board </a></li>
         <li><a href="../controllers/articleItems.php<?=$user?>">Auction bulletin board</a></li>
         <li><a href="../controllers/enrollment.php<?=$user?>">Auction Enrollment</li>
-        <li><a href="../controllers/auctiondetail.php<?=$user?>">Service center</a></li>
+        <li><a href="#">Service center</a></li>
       </ul>
     </nav>
     

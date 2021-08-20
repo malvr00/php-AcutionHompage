433bc10c867ddfe21ce7d14734de611692d6f3d2
malvr00 <77275513+malvr00@users.noreply.github.com>
2021-08-20 21:02:37 +0900

    charging point page Query modify

--- controllers/charging.php
+++ controllers/charging.php
@@ -7,8 +7,22 @@
     $check = htmlspecialchars($_POST['check'], ENT_QUOTES, 'UTF-8');  // 충전 버튼 Click 확인
     
     if(!empty($check)){
-      $point = intval(htmlspecialchars($_POST['pay'], ENT_QUOTES, 'UTF-8'));  // 충전금액 Get
-      $outString = "Click ok";
+      $payPoint = intval(htmlspecialchars($_POST['pay'], ENT_QUOTES, 'UTF-8'));  // 충전금액 Get
+      $usersFunction = new Userfunction($pdo);          // SQL Class 생성
+      
+    // **************** 유저 가지고있던 point 불러옴 **************** //
+      $sql = 'SELECT `infor_point` FROM `userInfor` WHERE `user_id` = \'' . $_GET['id'] . '\'';
+      $userPoint = $usersFunction->seachQuery($sql);
+    // **************** 유저 point Update **************** //
+      $totalPoint = $payPoint + (int)$userPoint[0]['infor_point'];
+      $sql = 'UPDATE `userInfor` SET `infor_point` = :infor_point WHERE `user_id` = :user_id';
+      $param = [
+        'infor_point'=>$totalPoint,
+        'user_id'=>$_GET['id']
+      ];
+      $usersFunction->uploadData($sql, $param);
+      
+      header('location: userdetail.php?id=' . $_GET['id']);
     }else{
       ob_start();
       include __DIR__ . '/../templates/charging.html.php';

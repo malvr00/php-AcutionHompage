7577bc4dc840c45274f5ec943280befd3b4c04ea
malvr00 <77275513+malvr00@users.noreply.github.com>
2021-09-01 20:07:30 +0900

    userdetail Secession Check Box ADD Fucntion

--- templates/userdetail.html.php
+++ templates/userdetail.html.php
@@ -4,8 +4,20 @@
     <span>Point : <?=$row['infor_point']?></span>
     <span>Enrollment : <?=$row['infor_auctionCnt']?> EA</span>
     <span>successful bid : <?=$row['infor_sfb']?> EA</span>
-    <form action="" method="POST">
-      <a href="../controllers/charging.php<?=$user?>"><input type="button" value="Point charging"> </a>
+    <form action="../controllers/userSecession.php<?=$user?>" method="POST">
+      <a href="../controllers/charging.php<?=$user?>"><input type="button" value="Point charging"></a>
+      <input type="submit" value="Secession" onclick="checkDelete()">
+      <input type="hidden" name="delete" id="delete" value="">
     </form>
   </div>
 <?php endforeach; ?>
+<script type="text/javascript">
+// È¸¿øÅ»Åð È®ÀÎ
+  function checkDelete(){
+    var check = confirm("Do you want to cancel membership?");
+    if(check)
+      document.getElementById("delete").value = "YES";
+    else
+      document.getElementById("delete").value = "NO";
+  }
+</script>

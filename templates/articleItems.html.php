9099ed1496443c99171f2eedebe61a8c2191e740
malvr00 <77275513+malvr00@users.noreply.github.com>
2021-09-09 23:01:08 +0900

    articleItems html Change

--- templates/articleItems.html.php
+++ templates/articleItems.html.php
@@ -1,11 +1,13 @@
-<?php foreach($result as $array) {?>
-  <?php if($array['article_end'] == '1'){?>
-    <div class="articles">
-      <a href="../controllers/auctiondetail.php?auction=<?=$array['article_id']?>&<?=$user?>">
-        <?php echo '<img src="data:image/bmp;base64,' . base64_encode( $array['article_image'] ) . '" />';?>
-        <h4><?php echo $array['article_title']; ?> </h4>
-      </a>
-      <span>View : <?=$array['article_views']?></span>
-    </div>
-  <?php }?>
-<?php } ?>
+<div class="articles-grid">
+  <?php foreach($result as $array) {?>
+    <?php if($array['article_end'] == '1'){?>
+      <div class="articles-grid_items">
+        <a href="../controllers/auctiondetail.php?auction=<?=$array['article_id']?>&<?=$user?>">
+          <?php echo '<img src="data:image/bmp;base64,' . base64_encode( $array['article_image'] ) . '" />';?>
+          <h4><?php echo $array['article_title']; ?> </h4>
+        </a>
+        <span>View : <?=$array['article_views']?></span>
+      </div>
+    <?php }?>
+  <?php } ?>
+</div>

4b7de41c5585542025d34c65e02dfe3c5ec5a9e4
malvr00 <77275513+malvr00@users.noreply.github.com>
2021-09-09 23:01:22 +0900

    auctiondetail html Change

--- templates/auctiondetail.html.php
+++ templates/auctiondetail.html.php
@@ -1,29 +1,27 @@
 <div class="auction-detail">
   <div class="auction-detail_bid">
     <?php echo '<img src="data:image/bmp;base64,' . base64_encode( $result1[0][3] ) . '" />';?>
-  </div>
-  <div class="auction-detail_Infor">
-    <div class="auction-detail_Infor_buy">
-      <div class="auction-detail_Infor_bidding">
-        <span>Real-time bidding  |</span>
-        <span><?=$buyuser[0]['user_id']?> : <?=$buyuser[0]['items_price']?> \</span>
+    <div class="auction-detail_bidding">
+      <h4 class="auction-detail_bidding_h4">Real-time bidding</h4>
+      <h4> <?=$buyuser[0]['user_id']?> : <?=$buyuser[0]['items_price']?> \</h4>
+      <div class="auction-detail_bidding_buy">
+        <form action="" method="post">
+          <input type="text" name="items_price" placeholder="bid price" class="auction-detail_bidding_buy__price">
+          <input type="submit" value="buy">
+          <br>
+          <input type="text" name="article_sell" placeholder="immediate sale price" class="auction-detail_bidding_buy__price">
+          <input type="submit" value="buy">
+        </form>
+        <?php if($userConfirm) {?>
+          <div class="auction-detail_bidding_buy__endBt">
+            <a href="../controllers/auctionDelete.php<?=$user?>&auction=<?=$auctionId?>"><input type="button" value="DELETE"></a>
+            <a href="../controllers/auctionEnd.php<?=$user?>&auction=<?=$auctionId?>">
+              <input type="button" value="END">
+            </a>
+          </div>
+        <?php }?>
       </div>
-      <form action="" method="post">
-        <input type="text" name="items_price" placeholder="bid price">
-        <input type="submit" value="buy">
-        <br>
-        <input type="text" name="article_sell" placeholder="immediate sale price">
-        <input type="submit" value="buy">
-      </form>
-      <?php if($userConfirm) {?>
-        <div class="auction-detail_Infor_endBt">
-          <a href="../controllers/auctionDelete.php<?=$user?>&auction=<?=$auctionId?>"><input type="button" value="DELETE"></a>
-          <a href="../controllers/auctionEnd.php<?=$user?>&auction=<?=$auctionId?>">
-            <input type="button" value="END">
-          </a>
-        </div>
-      <?php }?>
-      <aside>
+      <div class="auction-detail_bidding_detail">
         <table border="1">
           <tr>
             <td>Writer</td>
@@ -38,7 +36,7 @@
             <td><?=$result1[0][7]?>\</td>
           </tr>
         </table>
-      </aside>
+      </div>
     </div>
   </div>
 </div>

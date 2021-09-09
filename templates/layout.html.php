fc83930652cbe1f0dc4e0d5c9f89758567e034c7
malvr00 <77275513+malvr00@users.noreply.github.com>
2021-09-09 23:02:01 +0900

    layout html   typo correction

--- templates/layout.html.php
+++ templates/layout.html.php
@@ -1,27 +1,27 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
   <head>
     <meta charset="utf-8">
-    <link rel="stylesheet" href="../css/styles.css">
+    <link rel="stylesheet" href="../css/style.css" />
     <title><?=$title?></title>
   </head>
   <body>
     <header class="main-header">
+      <div class="login-bar-image">
+        <img src="../images/auctionImage.png" />
+        <h1><a href="../php/index.php<?=$user?>">Auction</a></h1>
+      </div>
       <div class="login-bar">
         <a href="<?=$userurl?>"><?=$userInform?></a>
         <a href="../controllers/userInOut.php?signtype=<?=$sgintype2?>"><?=$userInOut?></a>
       </div>
-      <h1><a href="../php/index.php<?=$user?>">Auction</a></h1>
     </header>
     
     <nav class="status-bar">
       <ul>
         <li><a href="../controllers/writing.php<?=$user?>">User bulletin board </a></li>
-        <li><a href="../controllers/articleItems.php<?=$user?>">Auction bulletin board</a></li>
-        <li>
-          <div class="status-bar_category">
-            <a href="../controllers/enrollment.php<?=$user?>">Auction Enrollment</a>
-          </div>
+        <li class="status-bar_category">
+          <a href="../controllers/articleItems.php<?=$user?>">Auction bulletin board</a>
           <div class="status-bar_category_m1">
             <ul>
               <li><a href="../controllers/categoryMenu.php<?=$user?>&cate=toy">Toy</a></li>
@@ -31,6 +31,9 @@
             </ul>
           </div>
         </li>
+        <li>
+          <a href="../controllers/enrollment.php<?=$user?>">Auction Enrollment</a>
+        </li>
       </ul>
     </nav>
     
@@ -39,7 +42,8 @@
     </main>
     
     <footer class="footer">
-      <h4> 2021. 08 &copy yong il<h4>
+      <h4> 2021. 08 &copy yong il</h4>
     </footer>
   </body>
+  <script src="../js/dispaly.js"></script>
 </html>

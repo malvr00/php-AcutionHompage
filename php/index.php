6dfd126a3ff337d71472bbdf8ef01c1ca749bfed
malvr00 <77275513+malvr00@users.noreply.github.com>
2021-09-09 23:00:49 +0900

    index.php   / mainScrenn ADD

--- php/index.php
+++ php/index.php
@@ -1,16 +1,23 @@
 <?php
   try{
-    include_once __DIR__ .'/../includes/DbConnect.php'; // db 연결
+    include_once __DIR__ .'/../includes/DbConnect.php';         // db 연결
     include_once __DIR__ .'/../includes/userInformation.php';   // 로그인 유저정보
+    include_once __DIR__ .'/../includes/Userfunction.php';      // SQL Class
+    
+    $usersFunction = new Userfunction($pdo);                    // SQL Class 생성
     
     $title = Auction;
     
     if($_GET['sell'] == '2'){
       echo '<script name="javascript"> window.alert("congratulations. successful bid!!!!!");</script>';
     }
- // home 출력
-  //ob_start();
-    $outString = 'mainpage2';
+    
+  // ********** 등록된 물품 Seach ********** //
+    $result = $usersFunction->seachQuery('SELECT * FROM `article`');
+     
+    ob_start();
+    include __DIR__ .'/../templates/articleItems.html.php';
+    $outString = ob_get_clean();
   
   }catch(PDOException $e){
   // 임시

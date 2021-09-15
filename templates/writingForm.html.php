16e52145062759aa04a3e1f188825e53e16e194a
malvr00 <77275513+malvr00@users.noreply.github.com>
2021-09-15 23:26:12 +0900

    writingForm.html  / before after ADD & div

--- templates/writingForm.html.php
+++ templates/writingForm.html.php
@@ -1,14 +1,20 @@
-<?php foreach($result as $usersText): ?>
-  <div class="user-writing">
-    <span><?=$usersText['id']?>th</span>
-    <a href="../controllers/writingView.php<?=$user?>&pageid=<?=$usersText['id']?>"><?=$usersText['title']?></a>
-    <span>Writer:<?=$usersText['user_id']?></span>
-  </div>
-<?php endforeach; ?>
-  <?php for($i=$first; $i<=$last; $i++){?>
-    <a href="writing.php<?=$user?>&page=<?=$i?>"><?=$i?></a>
-  <?php }?>
-<form action="" method="post">
-    <input type="hidden" name="write" value="1">
-    <input type="submit" value="write">
-</form>
+<div class="writing-screen">
+  <?php foreach($result as $usersText): ?>
+    <div class="user-writing">
+      <span><?=$usersText['id']?>th</span>
+      <a href="../controllers/writingView.php<?=$user?>&pageid=<?=$usersText['id']?>"><?=$usersText['title']?></a>
+      <span>Writer:<?=$usersText['user_id']?></span>
+    </div>
+  <?php endforeach; ?>
+    <div class="writing-pages">
+      <a href="writing.php<?=$user?>&page=<?=$beforPage?>">이전</a>
+      <?php for($i=$first; $i<=$last; $i++){?>
+        <a href="writing.php<?=$user?>&page=<?=$i?>"><?=$i?></a>
+      <?php }?>
+      <a href="writing.php<?=$user?>&page=<?=$last + 1?>">다음</a>
+    </div>
+    <form action="" method="post">
+        <input type="hidden" name="write" value="1">
+        <input type="submit" value="write">
+    </form>
+</div>
